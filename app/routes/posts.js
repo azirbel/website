2ecec14ea8680dd9eaa2ff2bd61bfd25506a4601
Alex Zirbel <alexzirbel@gmail.com>
2015-10-07 22:37:05 -0700

    Remove existing site to prep for Jekyll site

--- app/routes/posts.js
+++
@@ -1,9 +0,0 @@
-import Ember from "ember";
-import DS from "ember-data";
-
-export default Ember.Route.extend({
-  model: function() {
-    return DS.DefaultStore.find('post');
-    //return this.store.find('post');
-  }
-});
