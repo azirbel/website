4a3d6eed5f133be7e74c9f18d8d5dad988ddfe88
Alex Zirbel <alexzirbel@gmail.com>
2016-02-13 16:27:36 -0800

    Restyle sidebar

--- gulp/vendor.js
+++ gulp/vendor.js
@@ -2,15 +2,19 @@ var gulp = require('gulp');
 var changed = require('gulp-changed');
 
 gulp.task('vendor:css', function() {
-    return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
-        .pipe(changed('staging/css'))
-        .pipe(gulp.dest('staging/css'));
+  return gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
+      .pipe(changed('staging/css'))
+      .pipe(gulp.dest('staging/css'));
 });
 
 gulp.task('vendor:fonts', function() {
-    return gulp.src('node_modules/font-awesome/fonts/fontawesome-webfont.woff2')
-        .pipe(changed('staging/fonts'))
-        .pipe(gulp.dest('staging/fonts'));
+  return gulp.src([
+    'node_modules/font-awesome/fonts/fontawesome-webfont.eot',
+    'node_modules/font-awesome/fonts/fontawesome-webfont.woff',
+    'node_modules/font-awesome/fonts/fontawesome-webfont.woff2',
+  ])
+    .pipe(changed('staging/fonts'))
+    .pipe(gulp.dest('staging/fonts'));
 });
 
 gulp.task('vendor', ['vendor:css', 'vendor:fonts']);
