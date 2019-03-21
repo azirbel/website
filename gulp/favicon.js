8360371e2a3822d7e55bf2eb2a13a4889bf8e8fc
Alex Zirbel <alexzirbel@gmail.com>
2019-03-20 21:21:41 -0700

    migrate: Remove old site

--- gulp/favicon.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var changed = require('gulp-changed');
-
-gulp.task('favicon', function () {
-  return gulp.src('src/favicon.ico')
-    .pipe(changed('staging/*.ico'))
-    .pipe(gulp.dest('staging'));
-});
--- gulp/fonts.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var changed = require('gulp-changed');
-
-gulp.task('fonts', function () {
-  return gulp.src('src/fonts/*.ttf')
-    .pipe(changed('staging/fonts'))
-    .pipe(gulp.dest('staging/fonts'));
-});
